b425016264ea0984f749423b967c03cea6d83132
Haplocorn <haplocorn@gmail.com>
2019-06-11 14:38:24 +0200

    Small changes

--- src/main/java/btp/hd/intercom_row/util/GeneralUtils.java
+++ src/main/java/btp/hd/intercom_row/util/GeneralUtils.java
@@ -3,17 +3,23 @@ package btp.hd.intercom_row.util;
 import btp.hd.intercom_row.Activity.MonitorActivity;
 import btp.hd.intercom_row.Activity.StencilActivity;
 import ibis.constellation.Context;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class GeneralUtils {
     public static String label(String label, String host, int executor) {
-      return label + "-" + host; //+ "-" + executor;
+      return label + "-" + host + "-" + executor;
     }
 
     public static Context stencilContext(String host, int executor) {
-      return new Context(label(StencilActivity.LABEL, host, executor));
+        String label = label(StencilActivity.LABEL, host, executor);
+        log.info("Creating context with label: {}", label);
+      return new Context(label);
     }
 
     public static Context monitorContext(String host) {
-      return new Context(MonitorActivity.LABEL + "-" + host);
+        String label = label(MonitorActivity.LABEL, host, 1)
+        log.info("Creating context with label: {}", label);
+        return new Context(label);
     }
 }
