90b545d8feebcf7f66bf7dd92ac70ebb3899a63d
Haplocorn <haplocorn@gmail.com>
2019-06-11 14:43:01 +0200

    Small changes

--- src/main/java/btp/hd/intercom_row/util/GeneralUtils.java
+++ src/main/java/btp/hd/intercom_row/util/GeneralUtils.java
@@ -13,13 +13,11 @@ public class GeneralUtils {
 
     public static Context stencilContext(String host, int executor) {
         String label = label(StencilActivity.LABEL, host, executor);
-        log.info("Creating context with label: {}", label);
       return new Context(label);
     }
 
     public static Context monitorContext(String host) {
         String label = label(MonitorActivity.LABEL, host, 1);
-        log.info("Creating context with label: {}", label);
         return new Context(label);
     }
 }
