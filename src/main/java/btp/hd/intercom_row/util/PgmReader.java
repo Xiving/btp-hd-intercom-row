92a4e12cc0c97505716a799f05cdfc1bfc4e6c84
Xiving <bf.remmers@gmail.com>
2019-06-15 17:14:11 +0200

    Fix

--- src/main/java/btp/hd/intercom_row/util/PgmReader.java
+++ src/main/java/btp/hd/intercom_row/util/PgmReader.java
@@ -5,59 +5,67 @@ import java.util.StringTokenizer;
 
 public class PgmReader {
 
-    private static final String TEMP = "plasma";
-    private static final String COND = "pat2";
+  private static final String TEMP = "plasma";
+  private static final String COND = "pat2";
 
-    public static double[][] getTempValues(String fileDir, int height, int width) {
-        return read(fileDir + "/" + TEMP, height, width);
+  public static double[][] getTempValues(String fileDir, int height, int width) {
+    try {
+      return read(fileDir + "/" + TEMP, height, width);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    public static double[][] getCondValues(String fileDir, int height, int width) {
-        return read(fileDir + "/" + COND, height, width);
+    return null;
+  }
+
+  public static double[][] getCondValues(String fileDir, int height, int width) {
+    try {
+      return read(fileDir + "/" + COND, height, width);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    private static double[][] read(String fileDir, int height, int width) {
-        double[][] matrix;
-        String fileName = String.format("%s_%dx%d.pgm", fileDir, height, width);
+    return null;
+  }
 
-        BufferedReader br = openBufferedReader(fileName);
+  private static double[][] read(String fileDir, int height, int width) throws IOException {
+    double[][] matrix;
+    String fileName = String.format("%s_%dx%d.pgm", fileDir, height, width);
 
-        try { // read dimensions
-            StringTokenizer dimensions = new StringTokenizer(br.readLine());
-            height = Integer.parseInt(dimensions.nextToken());
-            width = Integer.parseInt(dimensions.nextToken());
-        } catch (IOException e) {
-            System.err.println("Invalid integer found!");
-            System.exit(1);
+    BufferedReader br = openBufferedReader(fileName);
+    br.readLine(); // ignore "P2"?
+
+    StringTokenizer dimensions = new StringTokenizer(br.readLine());
+    height = Integer.parseInt(dimensions.nextToken());
+    width = Integer.parseInt(dimensions.nextToken());
+
+    matrix = new double[height][width];
+
+    try { // read values
+      for (int i = 0; i < height; i++) {
+        StringTokenizer row = new StringTokenizer(br.readLine());
+
+        for (int j = 0; j < width; j++) {
+          matrix[i][j] = Double.parseDouble(row.nextToken());
         }
-
-        matrix = new double[height][width];
-
-        try { // read values
-            for (int i = 0; i < height; i++) {
-                StringTokenizer row = new StringTokenizer(br.readLine());
-
-                for (int j = 0; j < width; j++) {
-                    matrix[i][j] = Double.parseDouble(row.nextToken());
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Invalid double found!");
-            System.exit(1);
-        }
-
-        return matrix;
+      }
+    } catch (IOException e) {
+      System.err.println("Invalid double found!");
+      System.exit(1);
     }
 
-    private static BufferedReader openBufferedReader(String fileName) {
-        try {
-            return new BufferedReader(new FileReader(fileName));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    return matrix;
+  }
 
-        return null; // ignore
+  private static BufferedReader openBufferedReader(String fileName) {
+    try {
+      return new BufferedReader(new FileReader(fileName));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
+
+    return null; // ignore
+  }
 
 //    private static Reader getResourceReader(String fileName) throws FileNotFoundException {
 //        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
